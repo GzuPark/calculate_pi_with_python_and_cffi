bc4046a0863ff8ef27e49a82e8f30a1aa9c8e387
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-06-07 20:06:53 +0900

    Apply pi formula with python script

--- calculate_native_python.py
+++ calculate_native_python.py
@@ -1,5 +1,19 @@
+from utils.timer import timer
+
+
+@timer
 def calculate_pi(n_iter: int) -> float:
-    return 3.14
+    result = 0.0
+    numerator = 4.0
+    denominator = 1.0
+    operation = 1.0
+
+    for _ in range(n_iter):
+        result += operation * (numerator / denominator)
+        denominator += 2.0
+        operation *= -1.0
+    
+    return result
 
 
 if __name__ == "__main__":
